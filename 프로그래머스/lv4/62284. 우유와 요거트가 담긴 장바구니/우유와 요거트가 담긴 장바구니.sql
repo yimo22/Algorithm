37424a8997a65a2116b728e0fdc8e72961d5734e
yimo22 <yimo22@naver.com>
2022-10-09 19:35:40 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -4,17 +4,29 @@
 # 결과는 장바구니의 아이디 순으로 
 
 
-SELECT distinct C.CART_ID
-FROM CART_PRODUCTS AS C
-WHERE C.CART_ID in (
-    SELECT A.CART_ID
-    FROM
-        (select cart_id
-        from cart_products
-        where name = 'Yogurt') AS A join 
-        (select cart_id
-        from cart_products
-        where name = 'Milk') AS B
-    ON A.CART_ID = B.CART_ID
-)
-ORDER BY CART_ID;
+# 1차 정답
+# SELECT distinct C.CART_ID
+# FROM CART_PRODUCTS AS C
+# WHERE C.CART_ID in (
+#     SELECT A.CART_ID
+#     FROM
+#         (select cart_id
+#         from cart_products
+#         where name = 'Yogurt') AS A join 
+#         (select cart_id
+#         from cart_products
+#         where name = 'Milk') AS B
+#     ON A.CART_ID = B.CART_ID
+# )
+# ORDER BY CART_ID;
+
+# 2차 정답
+SELECT B.CART_ID
+FROM (
+    SELECT distinct CART_ID, NAME
+    FROM CART_PRODUCTS
+    WHERE NAME = 'Milk' or NAME = 'Yogurt'
+    ) as B
+GROUP BY B.CART_ID
+HAVING count(B.CART_ID) >= 2
+ORDER BY B.CART_ID ASC;
