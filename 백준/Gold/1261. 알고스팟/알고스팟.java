4a8339c136ffa85dc3ed9a5c363c354b15a35d26
yimo22 <yimo22@naver.com>
2023-01-30 23:48:51 +0900

    [Gold IV] Title: 알고스팟, Time: 160 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,9 +1,9 @@
 
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -26,22 +26,23 @@ public class Main {
 		}
 
 		// start
-		Queue<Node> q = new LinkedList<>();
-		int[][] visited = new int[N][M];
+		PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> {
+			return a.wall - b.wall;
+		});
+		pq.add(new Node(0, 0, 0));
+		int[][] dist = new int[N][M];
 		for (int i = 0; i < N; i++)
-			Arrays.fill(visited[i], -1);
-		q.add(new Node(0, 0, 0));
-		visited[0][0] = 0;
+			Arrays.fill(dist[i], Integer.MAX_VALUE);
 
-		int answer = Integer.MAX_VALUE;
-		while (!q.isEmpty()) {
-			Node cur = q.poll();
-//			System.out.println(cur);
-			if(cur.x == N-1 && cur.y == M-1) {
-				answer = Math.min(cur.wall, answer);
+		dist[0][0] = 0;
+
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+
+			if (dist[cur.x][cur.y] < cur.wall)
 				continue;
-			}
-			
+
+			// 진행
 			for (int[] d : dir) {
 				int nx = cur.x + d[0];
 				int ny = cur.y + d[1];
@@ -49,19 +50,20 @@ public class Main {
 				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
 					continue;
 
-				if ( (visited[nx][ny]==-1 || visited[nx][ny] > cur.wall) && map[nx][ny] == 0) {
-					visited[nx][ny] = cur.wall;
-					q.add(new Node(nx, ny, cur.wall));
+				if (map[nx][ny] == 0 && dist[nx][ny] > cur.wall) {
+					dist[nx][ny] = cur.wall;
+					pq.add(new Node(nx, ny, cur.wall));
 				}
 
-				if ( (visited[nx][ny] == -1 || visited[nx][ny] > cur.wall + 1) && map[nx][ny] == 1) {
-					visited[nx][ny] = cur.wall + 1;
-					q.add(new Node(nx, ny, cur.wall + 1));
+				if (map[nx][ny] == 1 && dist[nx][ny] > cur.wall + 1) {
+					dist[nx][ny] = cur.wall + 1;
+					pq.add(new Node(nx, ny, cur.wall + 1));
 				}
 			}
-
 		}
-		System.out.println(answer);
+
+		// min Value 출력
+		System.out.println(dist[N-1][M-1]);
 	}
 
 	static class Node {
@@ -78,7 +80,6 @@ public class Main {
 		public String toString() {
 			return "Node [x=" + x + ", y=" + y + ", wall=" + wall + "]";
 		}
-		
-		
+
 	}
 }
