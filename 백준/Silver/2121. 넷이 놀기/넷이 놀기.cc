7c3bd749cd63be2cee4fd7e94a46c61be7768795
yimo22 <yimo22@naver.com>
2023-04-24 22:36:49 +0900

    [Silver III] Title: 넷이 놀기, Time: 572 ms, Memory: 37396 KB -BaekjoonHub

--- 백준/Silver/2121. 넷이 놀기/넷이 놀기.cc
+++ 백준/Silver/2121. 넷이 놀기/넷이 놀기.cc
@@ -1,12 +1,43 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+
+#define endl '\n'
+
+int N, A, B;
+vector<pair<int, int>> v;
+map<pair<int, int>, int> mp;
+void Solve() {
+	for (int i = 0; i < N; i++) {
+		mp[v[i]]++;
+	}
+	// 완탐
+	int answer = 0;
+	for (int i = 0; i < v.size(); i++) {
+		int x = v[i].first;
+		int y = v[i].second;
+
+		if (mp.find({ x + A, y }) == mp.end()) continue;
+		if (mp.find({ x, y + B }) == mp.end()) continue;
+		if (mp.find({ x + A, y + B }) == mp.end()) continue;
+		answer++;
+	}
+	cout << answer << endl;
+}
+void init() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N >> A >> B;
+	int x, y;
+	for (int i = 0; i < N; i++) {
+		cin >> x >> y;
+		v.push_back({ x, y });
+	}
+}
+int main() {
+	init();
+	Solve();
+	return 0;
+}
