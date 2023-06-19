4ce51a47a80ab613c17c34b76c8df5fb7ff97861
yimo22 <yimo22@naver.com>
2023-06-19 16:49:29 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,33 +1,40 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+
 using namespace std;
+
+/* define */
+#define ll long long
 #define endl '\n'
 
-vector<int> arr(100'000, 0);
-vector<int> psum(100'000 + 1, 0);
+/* var */
 int N, K;
-void Solve() {
-	// getSum
-	for (int i = 0; i < N; i++) {
-		psum[i + 1] = psum[i] + arr[i];
-	}
-	int answer = -987'654'321;
-	for (int i = K; i <= N; i++) {
-		answer = max(psum[i] - psum[i - K], answer);
-	}
-	cout << answer << endl;
-}
+vector<int> v;
+vector<int> psum;
 void init() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> N >> K;
-	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
-	}
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N >> K;
+    v.resize(N);
+    psum.resize(N + 1);
+    for (int i = 0; i < N; i++) cin >> v[i];
+
+    // init
+    psum[0] = 0;
+    for (int i = 1; i <= N; i++) psum[i] = v[i - 1] + psum[i - 1];
+}
+void Solve() {
+
+    // 연속 K일의 최대 합
+    // i일을 기준으로 연속 K일의 합 : psum[i+K] - psum[i-1]
+    int answer = psum[1 + K - 1] - psum[0];
+    for (int i = 2; i + K - 1 <= N; i++) {
+        answer = max(answer, psum[i + K - 1] - psum[i - 1]);
+    }
+    cout << answer << endl;
 }
 int main() {
-	init();
-	Solve();
-	return 0;
+    init();
+    Solve();
+    return 0;
 }
