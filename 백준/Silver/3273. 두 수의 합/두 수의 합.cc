e9713899285614dd197171b76faacee3a24540aa
yimo22 <yimo22@naver.com>
2023-06-23 16:20:30 +0900

    [Silver III] Title: 두 수의 합, Time: 12 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,45 +1,42 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <unordered_map>
+#include <string>
+#include <cstring>
 using namespace std;
-#define endl '\n'
-int N, X;
-vector<int> arr;
-unordered_map<int, int> mp;
-void Solve() {
-	sort(arr.begin(), arr.end());
-	
-	int answer = 0;
-	for (int i = 0, j = N - 1; i < j;) {
-		int sum = arr[i] + arr[j];
 
-		if (sum == X) {
-			answer++;
-			i++;
-		}
-		else if (sum < X) {
-			i++;
-		}
-		else {
-			j--;
-		}
-	}
-	cout << answer << endl;
-}
+/* define */
+#define ll long long
+#define endl '\n'
+
+/* var */
+int N, x;
+vector<int> arr;
 void init() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> N;
-	int w;
-	for (int i = 0; i < N; i++) {
-		cin >> w;
-		arr.push_back(w);
-	}
-	cin >> X;
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    arr.resize(N);
+    for (int i = 0; i < N; i++) cin >> arr[i];
+    cin >> x;
+}
+void Solve() {
+    sort(arr.begin(), arr.end());
+    int s = 0, e = arr.size() - 1;
+    int ans = 0;
+    while (s < e) {
+        int sum = arr[s] + arr[e];
+        
+        if (sum == x) {
+            ans++;
+            e--;
+        }
+        else if (sum > x) e--;
+        else s++;
+    }
+    cout << ans << endl;
 }
 int main() {
-	init();
-	Solve();
-	return 0;
+    init();
+    Solve();
+    return 0;
 }
