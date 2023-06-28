7748d413f9b53654d91ff18ad285e42ef860564f
yimo22 <yimo22@naver.com>
2023-06-28 23:35:25 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 36 ms, Memory: 6764 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.cc
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.cc
@@ -3,68 +3,43 @@
 #include <vector>
 #include <string>
 #include <cstring>
+#include <tuple>
 using namespace std;
 
 /* define */
 #define ll long long
 #define endl '\n'
-
+bool comp(tuple<int, int, int>& a, tuple<int, int, int>& b) {
+    return get<0>(a) < get<0>(b);
+}
 /* var */
 int N;
 vector<int> arr;
 void Solve() {
     // 두 합이 최소가 되는 두쌍
-    sort(arr.begin(), arr.end());
     int answer = 0x7fffffff;
+    vector<tuple<int, int, int>> v;
     for (int i = 0; i < N; i++) {
         for (int j = i + 1; j < N; j++) {
-            int tgt = arr[i] + arr[j];
+            v.push_back({ arr[i] + arr[j], i, j });
+        }
+    }
+    sort(v.begin(), v.end(), comp);
 
-            int s = 0;
-            int e = N - 1;
-
-            while (s == i || s == j) s++;
-            while (e == i || e == j) e--;
-
-            int startSum = arr[s] + arr[e];
-            if (tgt == startSum) {
-                answer = 0;
-                goto EX1;
-            }
-
-            while (s < e) {
-                if (s == i || s == j) {
-                    s++;
-                    continue;
-                }
-                if (e == i || e == j) {
-                    e--;
-                    continue;
-                }
-
-                int sum = arr[s] + arr[e];
-                if (tgt < startSum) {
-                    answer = min(answer, abs(tgt - sum));
-                    if (sum > tgt) {
-                        e--;
-                    }
-                    else {
-                        s++;
-                    }
-                }
-                else {
-                    answer = min(answer, abs(tgt - sum));
-                    if (sum < tgt) {
-                        s++;
-                    }
-                    else {
-                        e--;
-                    }
-                }
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = i + 1; j < v.size(); j++) {
+            int o1_x, o1_y, o2_x, o2_y;
+            int o1_sum, o2_sum;
+            tie(o1_sum, o1_x, o1_y) = v[i];
+            tie(o2_sum, o2_x, o2_y) = v[j];
+            
+            if (o1_x != o2_x && o1_y != o2_y && o1_x != o2_y && o1_y != o2_x) {
+                answer = min(answer, abs(o1_sum - o2_sum));
+                break;
             }
         }
     }
-EX1:    cout << answer << endl;
+    cout << answer << endl;
 }
 void init() {
     ios_base::sync_with_stdio(0); cin.tie(0);
