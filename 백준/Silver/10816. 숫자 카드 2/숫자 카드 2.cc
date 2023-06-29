cbb9f87c0093ac89467dc9f7d2245ad63acde92f
yimo22 <yimo22@naver.com>
2023-06-29 23:06:57 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 3980 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cstring>
+using namespace std;
+
+/* define */
+#define ll long long
+#define endl '\n'
+
+/* var */
+int N, M;
+vector<int> arr;
+void Solve() {
+    cin >> M;
+    int w;
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < M; i++) {
+        cin >> w;
+        int pos = lower_bound(arr.begin(), arr.end(), w) - arr.begin();
+        int pos2 = upper_bound(arr.begin(), arr.end(), w) - arr.begin();
+        if (pos > pos2) 
+            cout << "0 ";
+        
+        else if (pos != N && arr[pos] == w) cout << pos2 - pos << " ";
+        else cout << "0 ";
+    }
+}
+void init() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    arr.resize(N);
+    for (int i = 0; i < N; i++) cin >> arr[i];
+}
+int main() {
+    init();
+    Solve();
+    return 0;
+}
