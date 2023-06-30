1beb2dc23b442ffa4818b9ab9e130d68d9b5e96d
yimo22 <yimo22@naver.com>
2023-06-30 22:15:04 +0900

    [Gold IV] Title: 행성 연결, Time: 196 ms, Memory: 15288 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.cc
+++ 백준/Gold/16398. 행성 연결/행성 연결.cc
@@ -1,57 +1,75 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <queue>
-#include <cmath>
-#define MAX 1'000
+#include <string>
+#include <cstring>
 using namespace std;
 
-int map[MAX][MAX];
+/* define */
+#define ll long long
+#define endl '\n'
+class Edge {
+public:
+    int s, e;
+    int cost;
+    Edge(int S, int E, int C) : s(S), e(E), cost(C) {}
+};
+bool comp(Edge & a, Edge & b) {
+    return a.cost < b.cost;
+}
+/* var */
 int N;
-typedef struct Node {
-	int idx, w;
-	Node(int I, int W) : idx(I), w(W) {}
-};
-struct comp {
-	bool operator()(Node & a, Node & b) {
-		return a.w > b.w;
-	}
-};
+int path[1000+1][1000+1];
+int parent[1000 + 1];
+int getp(int v) {
+    if (v == parent[v]) return v;
+    else return parent[v] = getp(parent[v]);
+}
+int uni(int a, int b) {
+    int pa = getp(a);
+    int pb = getp(b);
+    if (pa == pb) return 0;
+
+    if (pa < pb) {
+        parent[pb] = pa;
+    }
+    else parent[pa] = pb;
+
+    return 1;
+}
+void Solve() {
+    // init
+    for (int i = 1; i <= N; i++) parent[i] = i;
+
+    vector<Edge> edges;
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            if(path[i][j] > 0)
+                edges.push_back(Edge(i + 1, j + 1, path[i][j]));
+        }
+    }
+    sort(edges.begin(), edges.end(), comp);
+    ll answer = 0;
+    for (Edge e : edges) {
+        if (uni(e.s, e.e)) {
+            answer = (answer + e.cost);
+        }
+    }
+    cout << answer << endl;
+
+}
+void init() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    parent[0] = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> path[i][j];
+        }
+    }
+}
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cin >> map[i][j];
-		}
-	}
-
-	// prim
-	priority_queue<Node, vector<Node>, comp> pq; // 최소힙
-	vector<int> visited(N, 0);
-	
-	pq.push(Node(0, 0));
-	long long res = 0;
-	while (!pq.empty()) {
-		Node cur = pq.top(); pq.pop();
-
-		if (visited[cur.idx]) continue;
-		res += cur.w;
-		visited[cur.idx] = 1;
-
-		for (int i = 0; i < N; i++) {
-			if (!visited[i] && map[cur.idx][i] > 0) {
-				pq.push(Node(i, map[cur.idx][i]));
-			}
-		}
-	}
-
-	cout << res;
-
-
-
-
-	return 0;
+    init();
+    Solve();
+    return 0;
 }
