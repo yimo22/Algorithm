dbb53f6af4ddc49d5c763c4202712d57b898c234
yimo22 <yimo22@naver.com>
2023-07-15 19:53:40 +0900

    [Gold V] Title: 퇴사 2, Time: 248 ms, Memory: 66352 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -1,71 +1,35 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <string>
 #include <cstring>
 using namespace std;
 
-/* define*/
-#define endl '\n'
+/* define */
 #define ll long long
-#define MAX 15
+#define endl '\n'
 
-/* var*/
-ll dp[1'500'000+10];
+/* var */
 int N;
-int v[1'500'010][2];
-int answer = 0;
-/* 메모 사용*/
-ll dfs3(int depth) {
-	if (depth > N) return -987654321;
-	if (depth == N) {
-		return 0;
-	}
-
-	if (dp[depth] != -1) return dp[depth];
-
-	return dp[depth] = max(dfs3(depth + 1), dfs3(depth + v[depth][0]) + v[depth][1]);
-}
-
-/* 재귀(완탐) */
-int dfs2(int depth) {
-	if (depth > N) return -987654321; // 불가한 경우
-	if (depth == N) {
-		return 0;
-	}
-
-	return max(dfs2(depth + 1), dfs2(depth + v[depth][0]) + v[depth][1]);
-}
-/* 완탐 */
-int dfs(int depth, int cost) {
-	if (depth > N) return 0;
-	if (depth == N) {
-		answer = max(answer, cost);
-		return 0;
-	}
-
-	// 사용
-	dfs(depth + v[depth][0], cost + v[depth][1]);
-
-	// 미사용
-	dfs(depth + 1, cost);
+int arr[1500000+1][2];
+int dp[1500000 + 1]; // [i] : i일 ~ N일까지 일을 할때 최대수익
+int recur(int pos) {
+    if (pos > N) return -9999999999; // 불가
+    if (pos == N) return 0;
+    if (dp[pos] != -1) return dp[pos];
+    return dp[pos] = max(recur(pos + arr[pos][0]) + arr[pos][1], recur(pos + 1));
 }
 void Solve() {
-	memset(dp, -1, sizeof dp);
-	//dfs(0, 0);
-	//cout << answer << endl;
-	cout << dfs3(0) << endl;
+    memset(dp, -1, sizeof dp);
+    cout << recur(0);
 }
 void init() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> N;
-	int t, p;
-	for (int i = 0; i < N; i++) {
-		cin >> v[i][0] >> v[i][1];
-	}
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> arr[i][0] >> arr[i][1];
 }
 int main() {
-	init();
-	Solve();
-	return 0;
+    init();
+    Solve();
+    return 0;
 }
