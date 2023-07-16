443d81c9758b78e1c07c852d35933abed997ddb7
yimo22 <yimo22@naver.com>
2023-07-16 17:19:43 +0900

    [Gold IV] Title: 구슬게임, Time: 16 ms, Memory: 3980 KB -BaekjoonHub

---
+++ 백준/Gold/2600. 구슬게임/구슬게임.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cstring>
+using namespace std;
+
+/* define */
+#define ll long long
+#define endl '\n'
+//#define DEBUG 0
+/* var */
+int dp[2][500+1][500 + 1]; // player, k1 남은수, k2남은수
+int b[3];
+
+
+int recur(int turn, int k1, int k2) {
+    if (k1 == 0 && k2 == 0) {
+#ifdef DEBUG
+        printf("\t기저조건 : turn(%d), k1(%d), k2(%d), return(%d)\n", turn, k1, k2, 1 - turn);
+#endif // DEBUG
+        return dp[turn][k1][k2] = 1 - turn; // 상대방 승리
+    }
+    if (dp[turn][k1][k2] != -1) {
+#ifdef DEBUG
+        printf("\t기저조건 : turn(%d), k1(%d), k2(%d), return(%d)\n", turn, k1, k2, dp[turn][k1][k2]);
+#endif // DEBUG
+        return dp[turn][k1][k2];
+    }
+
+    int result;
+#ifdef DEBUG
+        printf("visited | turn(%d), k1(%d), k2(%d), return(%d)\n", turn, k1, k2, dp[turn][k1][k2]);
+#endif // DEBUG
+    // 반드시 자기가 이기는 경우에만 갱신
+    // 통1 에서 뺌
+    for (int i = 0; i < 3; i++) {
+        if (k1 - b[i] >= 0) {
+            if (recur(1 - turn, k1 - b[i], k2) == turn) {
+#ifdef DEBUG
+                printf("\tturn(%d), k1(%d), k2(%d), return(%d)\n", turn, k1, k2, turn);
+#endif // DEBUG
+                return dp[turn][k1][k2] = turn;
+            }
+        }
+    }
+    // 통2 에서 뺌
+    for (int i = 0; i < 3; i++) {
+        if (k2 - b[i] >= 0) {
+            if (recur(1 - turn, k1, k2 - b[i]) == turn) {
+#ifdef DEBUG
+                printf("\tturn(%d), k1(%d), k2(%d), return(%d)\n", turn, k1, k2, turn);
+#endif // DEBUG
+                return dp[turn][k1][k2] = turn;
+            }
+        }
+    }
+#ifdef DEBUG
+    printf("\tturn(%d), k1(%d), k2(%d), return(%d)\n", turn, k1, k2, 1-turn);
+#endif // DEBUG
+    return dp[turn][k1][k2] = 1 - turn;
+}
+void Solve() {
+    memset(dp, -1, sizeof dp);
+    int rep = 5;
+    int k[2];
+    while (rep > 0) {
+        cin >> k[0] >> k[1];
+        int ans = recur(0, k[0], k[1]);
+
+        if (ans) cout << "B";
+        else cout << "A";
+        cout << endl;
+        rep--;
+    }
+    
+}
+void init() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    for (int i = 0; i < 3; i++) cin >> b[i];
+}
+int main() {
+    init();
+    Solve();
+    return 0;
+}
